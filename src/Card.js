26202e7cea15576e61aa683b11640bbbf1d37816
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2023-12-20 18:38:59 +0300

    task_done

---
+++ src/Card.js
@@ -0,0 +1,10 @@
+import "./App.css";
+function Card() {
+  return (
+    <div className="App-Headers">
+      <h1>CVS</h1>
+      <p>Your go to Grocery store!</p>
+    </div>
+  );
+}
+export { Card };
