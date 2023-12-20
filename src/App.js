e45c7c9fc1c68e8603cd19a2579f91bd52613023
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2023-12-20 20:38:30 +0300

    task done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import { Card } from "./Card";
 import cvs from "./assets/cvs.png";
+import { CardPrices } from "./Card";
+import advil from "./assets/advil.jpeg";
+import asprin from "./assets/asprin.jpeg";
 function App() {
+  const prices = [
+    {
+      label: "Asprin",
+      price: "5KD",
+      pic: asprin,
+    },
+    {
+      label: "Advil",
+      price: "7KD",
+      pic: advil,
+    },
+  ];
+
   return (
     <div className="App-header">
       <Card />
+
       <img src={cvs} width="1200px" />
+      <div className="Card-style">
+        {prices.map((price) => (
+          <CardPrices label={price.label} price={price.price} pic={price.pic} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,21 @@
 import "./App.css";
 function Card() {
   return (
-    <div className="App-Headers">
+    <div className="Card-headers">
       <h1>CVS</h1>
       <p>Your go to Grocery store!</p>
     </div>
   );
 }
-export { Card };
+
+function CardPrices({ label, price, pic }) {
+  return (
+    <div>
+      <img src={pic} width="300px" />
+      <h4>{label}</h4>
+      <p>{price}</p>
+    </div>
+  );
+}
+
+export { Card, CardPrices };
